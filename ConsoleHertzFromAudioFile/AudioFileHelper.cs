9372cb948c8ea845ca1a0b6087613acb89e6a2cd
merto.loick <merto.loick@gmail.com>
2022-03-21 18:43:14 -0300

    first commit : test

---
+++ ConsoleHertzFromAudioFile/AudioFileHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for AudioFileHelper
+/// </summary>
+public static class AudioFileHelper
+{
+	public static bool Open(string path)
+	{
+		SoundPlayer player = new SoundPlayer(@"c:\mywavfile.wav");
+		player.Play();
+	}
+}
